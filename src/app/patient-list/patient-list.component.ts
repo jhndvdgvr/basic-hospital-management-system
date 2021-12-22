4eb9a63e9e45c1950fbd817eb5c839bfaf8cab55
David <jhndvdgvr>
2021-12-23 00:21:22 +0800

    added modal template for add, edit and delete

--- src/app/patient-list/patient-list.component.ts
+++ src/app/patient-list/patient-list.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientListComponent implements OnInit {
 
-  constructor() { }
+  isVisible = false;
+
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
   }
 
 }
